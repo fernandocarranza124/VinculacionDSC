ebb54722c673544b14e9d70f142f1b2c0761c2f4
FernandoCarranza <fernandocarranza124@gmail.com>
2020-09-24 15:37:32 -0500

    Vistas generales catalogo

---
+++ resources/views/catalogo.blade.php
@@ -0,0 +1,42 @@
+<!DOCTYPE html>
+<html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
+    <head>
+        <meta charset="utf-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+
+        <title>Laravel</title>
+
+        <!-- Fonts -->
+        <link href="https://fonts.googleapis.com/css2?family=Nunito:wght@400;600;700&display=swap" rel="stylesheet">
+        <link rel="stylesheet" type="text/css" href="css/bootstrap.css">
+
+        <!-- Styles -->
+    </head>
+    <body class="antialiased">
+        <div class="body-container animated fadeIn"><div class="container-fluid">
+            <br>
+            <br>
+            <div class="row">
+                <div class="col-sm-8 col-sm-offset-2">
+                    <div class="panel panel-default">
+                        <div class="panel-heading">
+                            <h3 class="panel-title text-center">
+                                Bienvenido de nuevo FERNANDO ISACC CARRANZA SALAZAR
+                            </h3>
+                        </div>
+                        <div class="panel-body text-center">
+                            <h4>
+                                (16121015)
+                                <br>FERNANDO ISACC CARRANZA SALAZAR
+                            </h4>
+                            <p>
+                                Ahora puedes visualizar todos los datos de tus materias actuales, tales como horario, calificaciones y temario de cada una de ellas en la sección "Periodo actual -&gt; Mis cursos".
+                            </p>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+    </body>
+</html>
---
+++ resources/views/tarjetas.blade.php
@@ -0,0 +1,89 @@
+<!DOCTYPE html>
+<html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
+    <head>
+        <meta charset="utf-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+
+        <title>Laravel</title>
+
+        <!-- Fonts -->
+        <link href="https://fonts.googleapis.com/css2?family=Nunito:wght@400;600;700&display=swap" rel="stylesheet">
+        <link rel="stylesheet" type="text/css" href="css/bootstrap.css">
+
+        <!-- Styles -->
+    </head>
+    <body class="antialiased">
+        <div class="body-container animated fadeIn"><div class="container-fluid">
+            <br>
+            <br>
+            <div class="row">
+                
+            </div>
+            <style type="text/css" media="screen">
+                img {
+                    float: left;
+    width:  100px;
+    height: 100px;
+    object-fit: cover;
+                }
+            </style>
+            <div class="row">
+                <div class="col-sm-3 col-sm-offset-1">
+                    <div class="panel panel-default">
+                        <div class="panel-heading">
+                            <h3 class="panel-title text-center">
+                                Agile Development Design + Value
+                            </h3>
+                        </div>
+                        <div class="panel-body text-center">
+                            <img src="img/residenciasEjemplo3.jpg" alt="">
+                            <p><span class="badge badge-secondary">Arquitectura web</span>
+                            <span class="badge badge-secondary">Ingenieria de software</span>
+                        </p>
+                        </div>
+                        <div class="panel-footer text-right">
+                            <button type="submit" class="btn btn-primary btn-raised btn-sm">Ver</button>
+                        </div>
+                    </div>
+                </div>
+                <div class="col-sm-3 col-sm-offset-0">
+                    <div class="panel panel-default">
+                        <div class="panel-heading">
+                            <h3 class="panel-title text-center">
+                                Secretaria de Desarrollo Economico del Estado de Michoacan
+                            </h3>
+                        </div>
+                        <div class="panel-body text-center">
+                            <img src="img/residenciasEjemplo1.png" alt="">
+                            <p><span class="badge badge-info">Arquitectura web</span>
+                            <span class="badge badge-primary">Ingenieria de software</span>
+                            <span class="badge badge-secondary">Seguridad informatica</span>
+                        </p>
+                        </div>
+                        <div class="panel-footer text-right">
+                            <button type="submit" class="btn btn-primary btn-raised btn-sm">Ver</button>
+                        </div>
+                    </div>
+                </div>
+                <div class="col-sm-3 col-sm-offset-0">
+                    <div class="panel panel-default">
+                        <div class="panel-heading">
+                            <h3 class="panel-title text-center">
+                                Direccion de Tecnologias de la Informacion y Comunicaciones
+                            </h3>
+                        </div>
+                        <div class="panel-body text-center">
+                            <img src="img/residenciasEjemplo2.jpg" alt="">
+                            <h6><span class="badge badge-info">Arquitectura web</span>
+                        </h6>
+                        </div>
+                        <div class="panel-footer text-right">
+                            <button type="submit" class="btn btn-primary btn-raised btn-sm">Ver</button>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+    </body>
+</html>
