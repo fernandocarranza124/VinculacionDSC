aa6b3638c211e2e9ff9de4d301d52bfb518f39af
FernandoCarranza <fernandocarranza124@gmail.com>
2020-09-26 10:05:44 -0500

    Catalogo sin imagenes

--- resources/views/tarjetas.blade.php
+++ resources/views/tarjetas.blade.php
@@ -7,83 +7,40 @@
         <title>Laravel</title>
 
         <!-- Fonts -->
-        <link href="https://fonts.googleapis.com/css2?family=Nunito:wght@400;600;700&display=swap" rel="stylesheet">
-        <link rel="stylesheet" type="text/css" href="css/bootstrap.css">
-
-        <!-- Styles -->
+         <meta charset="utf-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1">
+  <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.4.1/css/bootstrap.min.css">
+  <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.5.1/jquery.min.js"></script>
+  <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.4.1/js/bootstrap.min.js"></script>
     </head>
     <body class="antialiased">
-        <div class="body-container animated fadeIn"><div class="container-fluid">
-            <br>
-            <br>
-            <div class="row">
-                
-            </div>
-            <style type="text/css" media="screen">
-                img {
-                    float: left;
-    width:  100px;
-    height: 100px;
-    object-fit: cover;
-                }
-            </style>
-            <div class="row">
-                <div class="col-sm-3 col-sm-offset-1">
-                    <div class="panel panel-default">
-                        <div class="panel-heading">
-                            <h3 class="panel-title text-center">
-                                Agile Development Design + Value
-                            </h3>
-                        </div>
-                        <div class="panel-body text-center">
-                            <img src="img/residenciasEjemplo3.jpg" alt="">
-                            <p><span class="badge badge-secondary">Arquitectura web</span>
-                            <span class="badge badge-secondary">Ingenieria de software</span>
-                        </p>
-                        </div>
-                        <div class="panel-footer text-right">
-                            <button type="submit" class="btn btn-primary btn-raised btn-sm">Ver</button>
-                        </div>
-                    </div>
-                </div>
-                <div class="col-sm-3 col-sm-offset-0">
-                    <div class="panel panel-default">
-                        <div class="panel-heading">
-                            <h3 class="panel-title text-center">
-                                Secretaria de Desarrollo Economico del Estado de Michoacan
-                            </h3>
-                        </div>
-                        <div class="panel-body text-center">
-                            <img src="img/residenciasEjemplo1.png" alt="">
-                            <p><span class="badge badge-info">Arquitectura web</span>
-                            <span class="badge badge-primary">Ingenieria de software</span>
-                            <span class="badge badge-secondary">Seguridad informatica</span>
-                        </p>
-                        </div>
-                        <div class="panel-footer text-right">
-                            <button type="submit" class="btn btn-primary btn-raised btn-sm">Ver</button>
-                        </div>
-                    </div>
-                </div>
-                <div class="col-sm-3 col-sm-offset-0">
-                    <div class="panel panel-default">
-                        <div class="panel-heading">
-                            <h3 class="panel-title text-center">
-                                Direccion de Tecnologias de la Informacion y Comunicaciones
-                            </h3>
-                        </div>
-                        <div class="panel-body text-center">
-                            <img src="img/residenciasEjemplo2.jpg" alt="">
-                            <h6><span class="badge badge-info">Arquitectura web</span>
-                        </h6>
-                        </div>
-                        <div class="panel-footer text-right">
-                            <button type="submit" class="btn btn-primary btn-raised btn-sm">Ver</button>
-                        </div>
-                    </div>
-                </div>
-            </div>
+       <div class="container">
+  <h2>Modal Example</h2>
+  <!-- Trigger the modal with a button -->
+  <button type="button" class="btn btn-info btn-lg" data-toggle="modal" data-target="#myModal">Open Modal</button>
+
+  <!-- Modal -->
+  <div class="modal fade" id="myModal" role="dialog">
+    <div class="modal-dialog">
+    
+      <!-- Modal content-->
+      <div class="modal-content">
+        <div class="modal-header">
+          <button type="button" class="close" data-dismiss="modal">&times;</button>
+          <h4 class="modal-title">Modal Header</h4>
         </div>
+        <div class="modal-body">
+          <p>Some text in the modal.</p>
+        </div>
+        <div class="modal-footer">
+          <button type="button" class="btn btn-default" data-dismiss="modal">Close</button>
+        </div>
+      </div>
+      
     </div>
-    </body>
+  </div>
+  
+</div>
+
+     </body>
 </html>
--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -10,14 +10,8 @@
                 
             </div>
             <style type="text/css" media="screen">
-                .test {
-                    float: left;
-    width:  100px;
-    height: 100px;
-    object-fit: cover;}
-    . {
-        max-height: 100%;
-        overflow-y: scroll;
+    .vacante{
+        border-left:10px solid #04c496;
     }
                 
             </style>
@@ -29,12 +23,12 @@
                                 Agile Development Design + Value
                             </h3>
                         </div>
-                        <div class="panel-body text-center  ">
+                        <div class="panel-body text-center vacante">
                             <div class="row">
-                                <div class="col-md-6 col-xs-12">
+                                {{-- <div class="col-md-6 col-xs-12"> 
                                     <img class="test" src="img/residenciasEjemplo3.jpg" alt="">        
-                                </div>
-                                <div class="col-md-6 col-xs-12">
+                                </div>--}}
+                                <div class="col-12">
                                     <p>
                                         <span class="badge badge-secondary">Arquitectura web</span>
                                         <span class="badge badge-secondary">Ingenieria de software</span>
@@ -56,12 +50,12 @@
                                 Secretaria de Desarrollo Economico de Michoacan
                             </h3>
                         </div>
-                        <div class="panel-body text-center  ">
+                        <div class="panel-body text-center vacante" >
                             <div class="row">
-                                <div class="col-6">
+                                {{-- <div class="col-6"> 
                                     <img class="test" src="img/residenciasEjemplo1.png" alt="">
-                                </div>
-                                <div class="col-6">
+                                </div>--}}
+                                <div class="col-12">
                                     <p>
                                         <span class="badge badge-info">Arquitectura web</span>
                                         <span class="badge badge-primary">Ingenieria de software</span>
@@ -82,12 +76,12 @@
                                 Direccion de Tecnologias de la Informacion y Comunicaciones
                             </h3>
                         </div>
-                        <div class="panel-body text-center  ">
+                        <div class="panel-body text-center vacante">
                             <div class="row">
-                                <div class="col-6">
+                                {{-- <div class="col-6"> 
                                     <img class="test" src="img/residenciasEjemplo2.jpg" alt="">
-                                </div>
-                                <div class="col-6">
+                                </div>--}}
+                                <div class="col-12">
                                     <p>
                                         <span class="badge badge-info">Arquitectura web</span>
                                     </p>
