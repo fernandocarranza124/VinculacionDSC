d8f8ea36e6f8e9689bc19fb729eb70b7e28d0813
fernandocarranza124 <fernandocarranza124@gmail.com>
2021-02-16 10:13:27 -0600

    Update Authenticate.php

--- app/Http/Middleware/Authenticate.php
+++ app/Http/Middleware/Authenticate.php
@@ -1,7 +1,7 @@
 <?php
 
 namespace App\Http\Middleware;
-use Illuminate\Support\MessageBag;
+
 use Illuminate\Auth\Middleware\Authenticate as Middleware;
 
 class Authenticate extends Middleware
@@ -15,7 +15,7 @@ class Authenticate extends Middleware
     protected function redirectTo($request)
     {
         if (! $request->expectsJson()) {
-             $errors = new MessageBag(['password' => ['No Control o contraseña incorrecta.']]);
+             //$errors = new MessageBag(['password' => ['No Control o contraseña incorrecta.']]);
              
             return view('index')->withErrors($errors);
         }
